bbee765b21c6f5838d1ad9f408e501d941c75022
Enpu You <enpuyou@Enpus-MacBook-Pro.local>
2018-10-10 11:48:45 -0400

    r program and reflection first question update

--- src/Lab4Program.r
+++ src/Lab4Program.r
@@ -11,13 +11,52 @@ data(us_contagious_diseases)
 
 #Question 1.
 
+dat <- us_contagious_diseases %>% 
+    mutate(per100000rate = count * 100000 * weeks_reporting / (population * 52)) %>%
+    filter(disease == "Measles", state != "Alaska" & state != "Hawaii")
+
 #Question 2.
+
+ggplot(data = filter(dat, state == "California")) 
++ geom_point(mapping = aes(x = year, y = per100000rate)) 
++ geom_vline(xintercept = 1965)
 
 #Question 3.
 
+dat_caliFocus <- filter(us_contagious_diseases, state == "California")
+
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1950] <- "1950’s"
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1960] <- "1960’s"
+dat_caliFocus$yearBlock[dat_caliFocus$year == 1970] <- "1970’s"
+
+ggplot(data = dat_caliFocus) 
++ geom_bar(mapping = aes(x = state,y = count, fill = yearBlock), 
+           position = "dodge", stat = "identity") 
++ theme(axis.text.x = element_text(angle = 90,hjust = 1, vjust=-0.01))
+
+ggplot(data = dat_caliFocus)
++ geom_bar(mapping = aes(x = state,y = sqrt(count), fill = yearBlock), 
+           position = "dodge", stat = "identity") 
++ theme(axis.text.x = element_text(angle = 90,hjust = 1, vjust=-0.01))
+
 #Question 4.
 
+us_contagious_diseases$yearBlock[us_contagious_diseases$year == 1950] <- "1950’s"
+us_contagious_diseases$yearBlock[us_contagious_diseases$year == 1960] <- "1960’s"
+us_contagious_diseases$yearBlock[us_contagious_diseases$year == 1970] <- "1970’s"
+
+ggplot(data = us_contagious_diseases) 
++ geom_bar(mapping = aes(x = state,y = sqrt(count), fill = yearBlock), 
+           position = "dodge", stat = "identity") 
++ theme(axis.text.x = element_text(angle = 90,hjust = 1, vjust=-0.01))
+
 #Question 5.
+
+dat <- us_contagious_diseases %>% 
+  mutate(per100000rate = count * 100000 * weeks_reporting / (population * 52)) 
+
+ggplot(data = dat, mapping = aes(x = year,y = state)) 
++ geom_tile(mapping = aes(fill = per100000rate))
 
 #Question 6.
 
--- writing/reportAndReflections.md
+++ writing/reportAndReflections.md
@@ -1,9 +1,22 @@
-# Name
+# Name: Enpu
 # Date:
 
 Question 7.
 
+## Use data exploration to determine if other diseases (besides Measles) have
+##enough data to explore the effects of vaccines. Be critical of your online
+##research. Are you convinced by the primary or credible articles that you have
+##found? Be sure to cite all articles.
 
+I looked on WHO(World Health Organization) website for some vaccine data. And I
+found data on vaccines for [diphtheria](http://apps.who.int/gho/data/node.main.WHS3_41?lang=en)
+and [neonatal tetanus](http://apps.who.int/gho/data/view.main.1520_56?lang=en).
+You can see from the table, that there were dramatic decreasing in the number of
+cases reported from 20 years or, more obviously, 40 years before. I am convinced by
+these data since they are provided by WHO, which is a specialized agency of the
+United Nations. And these data were updated recently this passing summer.
+![Diphtheria](images/Diphtheria.png)
+![Neonatal tetanus](images/Neonatal tetanus.png)
 
 Question 8.
 
